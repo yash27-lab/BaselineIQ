f8fbc871fe3981aa5e68b49b42fd6b34d707ce8d
yash27-lab <54710562+yash27-lab@users.noreply.github.com>
2026-02-08 20:30:15 -0500

    Initial Commit

---
+++ BaselineIQ/BaselineIQApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaselineIQApp.swift
+//  BaselineIQ
+//
+//  Created by yash negi on 2/8/26.
+//
+
+import SwiftUI
+
+@main
+struct BaselineIQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BaselineIQ/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BaselineIQ
+//
+//  Created by yash negi on 2/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
